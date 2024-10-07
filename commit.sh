c5d5b256a383696c96b828c41eb1ce1700512f6b
snakem982 <snakem982@gmail.com>
2024-10-07 19:30:55 +0800

    commit

--- commit.sh
+++ commit.sh
@@ -1,9 +1,5 @@
 git config user.name "snakem982"
 git config user.email "snakem982@gmail.com"
-git checkout Alpha
-git pull
-git checkout moshen
-git merge Alpha
-git add .
+git add -A
 git commit -m "merge"
 git push
